5318c17076c4cf03273753ffdec3e27e6265b2f9
DDarkoO <ddejanoski@gmail.com>
2022-07-02 00:40:18 +0200

    BurgerApp homework

---
+++ Class_05 & Class_06/BurgerApp/BurgerApp/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BurgerApp.Models.Domain;
+using BurgerApp.Models.Enums;
+
+namespace BurgerApp.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public bool IsDelivered { get; set; }
+        //public int BurgerId { get; set; }
+        public string BurgerName { get; set; }
+        public string Location { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public int Id { get; set; }
+        //public Burger Burger { get; set; }
+    }
+}
